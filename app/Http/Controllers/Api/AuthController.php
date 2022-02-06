b634e05345ef617672a400f727b56b0bada1e366
johan wijanarko <67511127+johanWijanarko@users.noreply.github.com>
2022-02-06 13:11:48 +0700

    make auth copntroller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,13 +2,52 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-
+use Validator;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Api\BaseController as BaseController;
+// use App\Models\User;
 
 
 class AuthController extends Controller
 {
-    //
+    public function signIn(Request $request)
+    {
+        if(Auth::attemp([
+            'email' => $request->email, 
+            'password' => $request->password,
+        ]))
+        {
+            $authUser = Auth::user();
+            $success['token'] = $authUser->createToken('MyAuthApp')->plainTextToken;
+            $success['name'] = $authUser->name;
+
+            return $this->sendResponse($success, 'User signed in');
+        } else{
+            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+        }
+    }
+
+    public function signup(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Error validation', $validator->errors());       
+        }
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyAuthApp')->plainTextToken;
+        $success['name'] =  $user->name;
+   
+        return $this->sendResponse($success, 'User created successfully.');
+    }
 }
