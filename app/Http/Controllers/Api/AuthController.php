9082fdd1ebf387f3656f5e33aa193daa63d10438
johan wijanarko <67511127+johanWijanarko@users.noreply.github.com>
2022-02-06 12:56:15 +0700

    call baseController to describe response

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
+use App\Http\Controllers\Api\BaseController as BaseController;
+
 
 class AuthController extends Controller
 {
